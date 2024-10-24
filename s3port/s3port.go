44ceb00e2fcace7d014e19d859ce3c2433e035c2
mrovengerdev <rovenger.max@gmail.com>
2024-10-24 11:29:49 -0700

    docs(AWS-SDK): removed unnecessary aws sdk functions

--- s3port/s3port.go
+++ s3port/s3port.go
@@ -2,8 +2,6 @@ package s3port
 
 import (
 	"context"
-	"errors"
-	"io"
 	"log"
 	"os"
 	"path/filepath"
@@ -12,9 +10,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
-	"github.com/aws/aws-sdk-go-v2/service/s3/types"
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/smithy-go"
 	"github.com/joho/godotenv"
 )
 
@@ -105,101 +101,4 @@ func Upload() {
 
 		log.Printf("Uploaded the file: %s to S3 location: %s\n", path, result.Location)
 	}
-}
-
-// Lists the buckets in the current account.
-func (service AWSService) ListBuckets(ctx context.Context) ([]types.Bucket, error) {
-	result, err := service.S3Client.ListBuckets(ctx, &s3.ListBucketsInput{}) // I'm referencing a nil pointer
-	var buckets []types.Bucket
-	if err != nil {
-		log.Printf("Couldn't list buckets for your account. Here's why: %v\n", err)
-	} else {
-		buckets = result.Buckets
-	}
-	return buckets, err
-}
-
-// Lists the objects in a bucket.
-func (service AWSService) ListObjects(ctx context.Context, bucketName string) ([]types.Object, error) {
-	result, err := service.S3Client.ListObjectsV2(ctx, &s3.ListObjectsV2Input{
-		Bucket: aws.String(bucketName),
-	})
-	var contents []types.Object
-	if err != nil {
-		log.Printf("Couldn't list objects in bucket %v. Here's why: %v\n", bucketName, err)
-	} else {
-		contents = result.Contents
-	}
-	return contents, err
-}
-
-// Checks whether a bucket exists in the current account.
-func (service AWSService) BucketExists(ctx context.Context, bucketName string) (bool, error) {
-	_, err := service.S3Client.HeadBucket(ctx, &s3.HeadBucketInput{
-		Bucket: aws.String(bucketName),
-	})
-	exists := true
-	if err != nil {
-		var apiError smithy.APIError
-		if errors.As(err, &apiError) {
-			switch apiError.(type) {
-			case *types.NotFound:
-				log.Printf("Bucket %v is available.\n", bucketName)
-				exists = false
-				err = nil
-			default:
-				log.Printf("Either you don't have access to bucket %v or another error occurred. "+
-					"Here's what happened: %v\n", bucketName, err)
-			}
-		}
-	} else {
-		log.Printf("Bucket %v exists and you already own it.", bucketName)
-	}
-
-	return exists, err
-}
-
-// Reads from a file and puts the data into an object in a bucket.
-func (service AWSService) UploadFile(ctx context.Context, bucketName string, objectKey string, fileName string) error {
-	file, err := os.Open(fileName)
-	if err != nil {
-		log.Printf("Couldn't open file %v to upload. Here's why: %v\n", fileName, err)
-	} else {
-		defer file.Close()
-		_, err = service.S3Client.PutObject(ctx, &s3.PutObjectInput{
-			Bucket: aws.String(bucketName),
-			Key:    aws.String(objectKey),
-			Body:   file,
-		})
-		if err != nil {
-			log.Printf("Couldn't upload file %v to %v:%v. Here's why: %v\n",
-				fileName, bucketName, objectKey, err)
-		}
-	}
-	return err
-}
-
-// Gets an object from a bucket and stores it in a local file.
-func (basics AWSService) DownloadFile(ctx context.Context, bucketName string, objectKey string, fileName string) error {
-	result, err := basics.S3Client.GetObject(ctx, &s3.GetObjectInput{
-		Bucket: aws.String(bucketName),
-		Key:    aws.String(objectKey),
-	})
-	if err != nil {
-		log.Printf("Couldn't get object %v:%v. Here's why: %v\n", bucketName, objectKey, err)
-		return err
-	}
-	defer result.Body.Close()
-	file, err := os.Create(fileName)
-	if err != nil {
-		log.Printf("Couldn't create file %v. Here's why: %v\n", fileName, err)
-		return err
-	}
-	defer file.Close()
-	body, err := io.ReadAll(result.Body)
-	if err != nil {
-		log.Printf("Couldn't read object body from %v. Here's why: %v\n", objectKey, err)
-	}
-	_, err = file.Write(body)
-	return err
 }
